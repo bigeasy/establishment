f1befd4b296541b9682913c4689dc80ffba004e3
Alan Gutierrez <alan@prettyrobots.com>
2015-12-17 16:12:40 -0500

    Implement port probing.

--- establishment.js
+++ establishment.js
@@ -1,1 +1,14 @@
-module.exports = 1
+var cadence = require('cadence')
+var Delta = require('delta')
+
+module.exports = cadence(function (async, server, port, address) {
+    var loop = async([function () {
+        new Delta(async()).ee(server).on('listening')
+        server.listen(port, address)
+    }, /^EADDRINUSE$/, function () {
+        port++
+        return [ loop.continue ]
+    }], function () {
+        return [ loop.break ]
+    })()
+})
--- t/establishment/establishment.t.js
+++ t/establishment/establishment.t.js
@@ -1,5 +1,18 @@
-require('proof')(1, prove)
+require('proof')(1, require('cadence')(prove))
 
-function prove (assert) {
-    assert(require('../..'), 'require')
+function prove (async, assert) {
+    var net = require('net')
+    var establishment = require('../..')
+    var first, second
+    async(function () {
+        first = net.createServer()
+        establishment(first, 22000, '0.0.0.0', async())
+    }, function () {
+        second = net.createServer()
+        establishment(second, 22000, '0.0.0.0', async())
+    }, function () {
+        assert(first.address().port && first.address().port != second.address().port, 'bound')
+        first.close(async())
+        second.close(async())
+    })
 }
